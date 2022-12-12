14c9d4daf08e0aa4610fa56703b731ab51030e6e
Arinda Clever <48243275+cleverclope@users.noreply.github.com>
2022-12-12 09:55:45 +0300

    ngrx and rxjs

--- src/app/users-rxjs/components/users/users.component.ts
+++ src/app/users-rxjs/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { filter, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  users = [
+    { id: '1', name: 'John', isActive: true },
+    { id: '2', name: 'Jack', isActive: true },
+    { id: '2', name: 'Mike', isActive: true },
+  ];
+
+  // of
+  users$ = of(this.users);
+
+  // map no init
+  usernames$ = this.users$.pipe(map((users) => users.map((user) => user.name)));
+
+  // filter
+  filteredUsers$ = this.users$.pipe(
+    filter((users) => users.every((user) => user.isActive))
+  );
+
+
   constructor() { }
 
   ngOnInit(): void {
+
+  this.users$.subscribe((users) => {
+      console.log('user', users);
+    });
+
   }
 
 }
